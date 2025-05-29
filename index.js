f06fadad3947994135958e32932f5daa15e9d0a8
Don0716 <donmonteiro16@gmail.com>
2025-05-30 01:59:50 +0530

    feat: added routes and vercel file

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 
 const cors = require("cors");
 const corsOptions = {
@@ -17,6 +18,7 @@ const Lead = require("./models/lead.model");
 const SalesAgent = require("./models/salesAgent.model");
 const Comment = require("./models/comment.model");
 const Tag = require("./models/tag.model");
+const salesAgentModel = require("./models/salesAgent.model");
 
 // Add Sales Agent.
 app.post("/agents", async (req, res) => {
@@ -36,12 +38,77 @@ app.post("/agents", async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+// Get sales agents
+app.get("/agents", async (req, res) => {
+  try {
+    const agents = await SalesAgent.find();
+    if (agents.length > 0) {
+      res.json({ message: "Agents Found", agentsData: agents });
+    } else {
+      res.status(404).json({ error: "Agents not found." });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+// Delete Sales Agent.
+app.delete("/agents/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteAgent = await SalesAgent.findByIdAndDelete(id);
+    if (deleteAgent) {
+      res
+        .status(200)
+        .json({ message: "Agent deleted Successfully", agent: deleteAgent });
+    } else {
+      res.status(404).json({ error: `Agent with ID ${id} not Found. ` });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
 // Add Lead
 app.post("/leads", async (req, res) => {
   try {
     const { name, source, salesAgent, status, tags, timeToClose, priority } =
       req.body;
+
+    // Input validations (basic)
+    if (!name || typeof name !== "string") {
+      return res
+        .status(400)
+        .json({ error: "Invalid input: 'name' is required." });
+    }
+
+    if (
+      !source ||
+      ![
+        "Website",
+        "Referral",
+        "Cold Call",
+        "Advertisement",
+        "Email",
+        "Other",
+      ].includes(source)
+    ) {
+      return res
+        .status(400)
+        .json({ error: "Invalid input: 'source' is invalid." });
+    }
+
+    if (!mongoose.Types.ObjectId.isValid(salesAgent)) {
+      return res
+        .status(400)
+        .json({ error: "Invalid input: 'salesAgent' must be a valid ID." });
+    }
+
+    const agentExists = await SalesAgent.findById(salesAgent);
+    if (!agentExists) {
+      return res
+        .status(404)
+        .json({ error: `Sales agent with ID '${salesAgent}' not found.` });
+    }
 
     const newLead = new Lead({
       name,
@@ -55,9 +122,10 @@ app.post("/leads", async (req, res) => {
     const saveLead = await newLead.save();
 
     if (saveLead) {
+      await saveLead.populate("salesAgent", "name");
       res
         .status(201)
-        .json({ message: "Lead Added Successfully.", lead: saveLead });
+        .json({ message: "Lead Created Successfully.", lead: saveLead });
     } else {
       res.status(400).json({ error: "Failed to Add Lead." });
     }
@@ -65,36 +133,54 @@ app.post("/leads", async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
-
-// Post a comment to Lead
-app.post("/leads/:id/comments", async (req, res) => {
+// Update a Lead
+app.put("/leads/:id", async (req, res) => {
   try {
-    const { commentText, author } = req.body;
-    const leadId = req.params.id;
+    const { id } = req.params;
+    const updates = req.body;
 
-    const findLead = await Lead.findById(leadId);
-    if (findLead) {
-      const newComment = new Comment({
-        lead: leadId,
-        author,
-        commentText,
-      });
-      const saveComment = await newComment.save();
-      res.status(201).json({
-        message: "Created Comment Successfully.",
-        comment: saveComment,
-      });
+    if (updates.status === "Closed") {
+      updates.closedAt = new Date();
+    }
+    if (updates.status && updates.status !== "Closed") {
+      updates.closedAt = null;
+    }
+    const updateLead = await Lead.findByIdAndUpdate(id, updates, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (updateLead) {
+      res
+        .status(200)
+        .json({ message: "Lead Updated Successfully", updateLead });
     } else {
-      res.status(404).json({ error: "Lead does not exist." });
+      res.status(404).json({ error: "Lead not found." });
     }
   } catch (error) {
-    res.status(500).json(error.message);
+    res.status(500).json({ error: error.message });
   }
 });
-
+// Delete a Lead
+app.delete("/leads/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteLead = await Lead.findByIdAndDelete(id);
+    if (deleteLead) {
+      res
+        .status(200)
+        .json({ message: "Lead deleted Successfully", lead: deleteLead });
+    } else {
+      res.status(404).json({ error: `Lead with ID ${id} not Found. ` });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+// Get Leads and query parameters.
 app.get("/leads", async (req, res) => {
   try {
-    const { salesAgent, status, tags, source } = req.query;
+    const { salesAgent, status, tags, source, priority } = req.query;
     // input validation
     const validStatuses = [
       "New",
@@ -111,6 +197,7 @@ app.get("/leads", async (req, res) => {
       "Email",
       "Other",
     ];
+    const validPriority = ["High", "Medium", "Low"];
     const filter = {};
     // Validate salesAgent ObjectId
     if (salesAgent) {
@@ -133,6 +220,17 @@ app.get("/leads", async (req, res) => {
       filter.status = status;
     }
 
+    if (priority) {
+      if (!validPriority.includes(priority)) {
+        return res.status(400).json({
+          error: `invalid Input: "priority must be one of ${JSON.stringify(
+            validPriority
+          )} `,
+        });
+      }
+      filter.priority = priority;
+    }
+
     if (source) {
       if (!validSources.includes(source)) {
         return res.status(400).json({
@@ -146,10 +244,50 @@ app.get("/leads", async (req, res) => {
 
     if (tags) {
       const tagArray = Array.isArray(tags) ? tags : tags.split(",");
-      filter.tags = { $all: tagArray }; // Matches leads that have all provided tags
+
+      // Validate tags against database
+      const existingTags = await Tag.find({ name: { $in: tagArray } });
+      const validTagNames = existingTags.map((tag) => tag.name);
+      const invalidTags = tagArray.filter(
+        (tag) => !validTagNames.includes(tag)
+      );
+
+      if (invalidTags.length > 0) {
+        return res.status(400).json({
+          error: `Invalid tag(s): ${invalidTags.join(", ")}`,
+        });
+      }
+
+      filter.tags = { $all: tagArray }; // Only use validated tags
     }
 
-    const leads = await Lead.find().populate("salesAgent");
+    // Post a comment to Lead
+    app.post("/leads/:id/comments", async (req, res) => {
+      try {
+        const { commentText, author } = req.body;
+        const leadId = req.params.id;
+
+        const findLead = await Lead.findById(leadId);
+        if (findLead) {
+          const newComment = new Comment({
+            lead: leadId,
+            author,
+            commentText,
+          });
+          const saveComment = await newComment.save();
+          res.status(201).json({
+            message: "Created Comment Successfully.",
+            comment: saveComment,
+          });
+        } else {
+          res.status(404).json({ error: "Lead does not exist." });
+        }
+      } catch (error) {
+        res.status(500).json(error.message);
+      }
+    });
+
+    const leads = await Lead.find(filter).populate("salesAgent");
     if (leads.length > 0) {
       res.status(200).json(leads);
     } else {
@@ -160,10 +298,78 @@ app.get("/leads", async (req, res) => {
   }
 });
 
+// Comments
+// Post Comments.
+app.post("/leads/:id/comments", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { author, commentText } = req.body;
+    const lead = await Lead.findById(id);
+
+    // Validate Input
+    if (!commentText || typeof commentText !== "string") {
+      return res
+        .status(400)
+        .json({ error: `'commentText' is required and must be a string.` });
+    }
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({ error: "Invalid Lead ID." });
+    }
+    if (!lead) {
+      return res.status(404).json({ error: "Lead not Found." });
+    }
+    const newComment = new Comment({
+      lead: lead._id,
+      author: author,
+      commentText,
+    });
+    const saveComment = await newComment.save();
+    res
+      .status(201)
+      .json({ message: "Comment Added Successfully.", comment: saveComment });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+// Delete Comments.
+app.delete("/comments/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteComment = await Comment.findByIdAndDelete(id);
+
+    if (!deleteComment) {
+      return res.status(404).json({ error: "Failed to Delete." });
+    }
+
+    res.status(201).json({
+      message: "Comment Deleted Successfully.",
+      comment: deleteComment,
+    });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+// Get comments
+app.get("/leads/:id/comments", async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const comments = await Comment.find({ lead: id }).populate("author");
+    if (comments.length > 0) {
+      res.status(200).json(comments);
+    } else {
+      res.status(404).json({ error: "No Comments on this Lead." });
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// TAGS
 app.post("/tags", async (req, res) => {
   try {
     const name = req.body;
-    const newTag = new Tag({ name });
+    const newTag = new Tag(name);
     const saveTag = await newTag.save();
     if (saveTag) {
       res.status(201).json({ message: "Created Tag", tag: saveTag });
@@ -176,11 +382,81 @@ app.post("/tags", async (req, res) => {
 });
 app.get("/tags", async (req, res) => {
   try {
-    if (saveTag) {
-      res.status(201).json({ message: "Created Tag", tag: saveTag });
+    const tags = await Tag.find();
+    if (tags.length > 0) {
+      res.status(201).json({ message: "Created Tag", tag: tags });
     } else {
-      res.status(404).json({ error: "Failed to create Tag." });
+      res.status(404).json({ error: "No Tags Found." });
     }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// REPORT
+// Get leads closed last week.
+app.get("/report/last-week", async (req, res) => {
+  try {
+    const now = new Date();
+    const sevenDaysAgo = new Date();
+    sevenDaysAgo.setDate(now.getDate() - 7);
+    // Find Leads with status "Closed" and closedAt data within last 7 days.
+    const leads = await Lead.find({
+      status: "Closed",
+      closedAt: { $gte: sevenDaysAgo, $lte: now },
+    }).populate("salesAgent", "name"); // populate sales agents name
+    // Map resuslts to desired output
+    const formattedLeads = leads.map((lead) => ({
+      id: lead._id,
+      name: lead.name,
+      salesAgent: lead.salesAgent.name,
+      closedAt: lead.closedAt,
+    }));
+    res.status(200).json(formattedLeads);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+// Get Leads Closed by Sales Agent.
+app.get("/report/closed-by-agent", async (req, res) => {
+  try {
+    const closedLeads = await Lead.find({ status: "Closed" }).populate(
+      "salesAgent"
+    );
+    const leadCounts = {};
+
+    closedLeads.forEach((lead) => {
+      const agent = lead.salesAgent;
+      if (!agent) return; // skip if no agent for lead.
+
+      const agentId = agent._id.toString();
+      if (!leadCounts[agentId]) {
+        //create new entry if, agent is not there in leadCounts object.
+        leadCounts[agentId] = {
+          salesAgentId: agentId,
+          salesAgentName: agent.name,
+          closedLeadsCount: 0,
+        };
+      }
+      leadCounts[agentId].closedLeadsCount++;
+    });
+    const result = Object.values(leadCounts);
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+// Get all leads that aren't closed.
+app.get("/report/pipeline", async (req, res) => {
+  try {
+    const leads = await Lead.find();
+    let leadsCounter = 0;
+    leads.map((lead) => {
+      if (lead.status !== "Closed") {
+        leadsCounter += 1;
+      }
+    });
+    res.status(200).json({ totalLeadsInPipeline: leadsCounter });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
